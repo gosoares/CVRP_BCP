9b5dec1d3c2bc547f6c3fad0bc04927c05401fab
Gabriel Soares <gabriel@gosoares.com>
2024-12-26 00:33:54 -0300

    pricing starting implementation

---
+++ src/Pricing.cpp
@@ -0,0 +1,44 @@
+#include "Pricing.h"
+
+#include <limits>
+
+Pricing::Pricing(const Instance& instance)
+    : instance(instance)
+    , V(instance.getV())
+    , C(instance.getVehicleCapacity())
+    , minCost(C + 1, std::vector<double>(V))
+    , before(C + 1, std::vector<int64_t>(V)) {}
+
+void Pricing::solve(const std::vector<std::vector<double> >& prices) {
+    this->initialize();
+    int64_t totalDemand;
+    double totalCost;
+
+    for (int64_t d = 0; d < this->C; d++) {
+        for (int64_t v = 0; v < this->V; v++) {
+            if (this->minCost[d][v] == std::numeric_limits<double>::infinity()) continue;
+            for (int64_t w = 0; w < this->V; w++) {
+                if (v == w || w == this->before[d][v]) continue;  // eliminate 2-cycle q-routes
+                totalDemand = d + this->instance.getDemand(w);
+                totalCost = this->minCost[d][v] + prices[v][w];
+
+                if (totalDemand <= this->C && totalCost < this->minCost[totalDemand][w]) {
+                    this->minCost[totalDemand][w] = totalCost;
+                    this->before[totalDemand][w] = v;
+                }
+            }
+        }
+    }
+    this->minCost[0][0] = 0;
+}
+
+void Pricing::initialize() {
+    // Initialize minCost and before
+    for (int64_t d = 0; d <= this->C; d++) {
+        for (int64_t v = 0; v < this->V; v++) {
+            this->minCost[d][v] = std::numeric_limits<double>::infinity();
+            this->before[d][v] = -1;
+        }
+    }
+    this->minCost[0][0] = 0;
+}
---
+++ src/Pricing.h
@@ -0,0 +1,34 @@
+#ifndef PRICING_H
+#define PRICING_H
+
+#pragma once
+
+#include <vector>
+
+#include "Instance.h"
+
+class Pricing {
+   public:
+    Pricing(const Instance& instance);
+
+    void solve(const std::vector<std::vector<double> >& prices);
+
+    double getMinCost(int64_t d, int64_t v) const { return this->minCost[d][v]; }
+
+    int64_t getBefore(int64_t d, int64_t v) const { return this->before[d][v]; }
+
+   private:
+    const Instance& instance;
+    const int64_t V;  // Number of vertices (depot + V-1 customers)
+    const int64_t C;  // Vehicle capacity
+
+    // minCost[d][v] = least costly walk that reaches vertex v using total demand exactly d
+    std::vector<std::vector<double> > minCost;
+
+    // before[d][v] = vertex visited before v in least costly walk
+    std::vector<std::vector<int64_t> > before;
+
+    void initialize();
+};
+
+#endif
