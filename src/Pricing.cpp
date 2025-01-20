98584363d27b14109bd63f0fd6b546bc3ee1dea5
Gabriel Soares <gabriel@gosoares.com>
2025-01-20 20:50:45 -0300

    complete pricing with two labels

--- src/Pricing.cpp
+++ src/Pricing.cpp
@@ -2,43 +2,130 @@
 
 #include <limits>
 
+Labels::Labels(int64_t vertex, int64_t maxLabels)
+    : vertex(vertex)
+    , maxLabels(maxLabels) {};
+
+Labels::~Labels() {
+    for (Label* label : this->labels) {
+        delete label;
+    }
+    this->labels.clear();
+}
+
+void Labels::add(double cost, const Label* previous) {
+    size_t insertIndex = this->labels.size();
+    while (insertIndex > 0 && this->labels[insertIndex - 1]->cost > cost) {
+        insertIndex--;
+    }
+
+    if (this->labels.size() < this->maxLabels) {  // has space to insert without removing another
+        this->labels.insert(this->labels.begin() + insertIndex, new Label{this->vertex, cost, previous});
+        return;
+    }
+    if (insertIndex == this->labels.size()) return;  // its not better than any label
+
+    // shift labels to the right
+    delete this->labels.back();
+    for (size_t i = this->labels.size() - 2; i >= insertIndex; i--) {
+        this->labels[i + 1] = this->labels[i];
+    }
+
+    this->labels[insertIndex] = new Label{this->vertex, cost, previous};
+}
+
+const Label* Labels::getBestLabelToExtendTo(int64_t vertex) const {
+    int64_t index = 0;
+    while (this->labels[index]->previous->vertex == vertex) {
+        index++;
+    }
+    return this->labels[index];
+}
+
+void Labels::clear() {
+    for (Label* label : this->labels) {
+        delete label;
+    }
+    this->labels.clear();
+}
+
 Pricing::Pricing(const Instance& instance)
     : instance(instance)
     , V(instance.getV())
     , C(instance.getVehicleCapacity())
-    , minCost(C + 1, std::vector<double>(V))
-    , before(C + 1, std::vector<int64_t>(V)) {}
+    , M(C + 1, std::vector<Labels>()) {
+    for (int64_t d = 0; d <= this->C; d++) {
+        this->M[d].reserve(this->V);
+        for (int64_t v = 0; v < this->V; v++) {
+            this->M[d].emplace_back(0, 2);
+        }
+    }
+}
 
-void Pricing::solve(const std::vector<double>& prices) {
+std::vector<std::vector<int64_t> > Pricing::solve(const std::vector<double>& prices) {
     this->initialize();
     int64_t totalDemand;
     double totalCost;
 
+    // calculate the labels
     for (int64_t d = 0; d < this->C; d++) {
         for (int64_t v = 0; v < this->V; v++) {
-            if (this->minCost[d][v] == std::numeric_limits<double>::infinity()) continue;
             for (int64_t w = 0; w < this->V; w++) {
-                if (v == w || w == this->before[d][v]) continue;  // eliminate 2-cycle q-routes
-                totalDemand = d + this->instance.getDemand(w);
-                totalCost = this->minCost[d][v] + prices[instance.getEdgeId(v, w)];
+                if (v == w) continue;
 
-                if (totalDemand <= this->C && totalCost < this->minCost[totalDemand][w]) {
-                    this->minCost[totalDemand][w] = totalCost;
-                    this->before[totalDemand][w] = v;
-                }
+                totalDemand = d + this->instance.getDemand(w);
+                if (totalDemand > this->C) continue;
+
+                const Label* label = this->M[d][v].getBestLabelToExtendTo(w);
+                totalCost = label->cost + prices[instance.getEdgeId(v, w)];
+
+                this->M[totalDemand][w].add(totalCost, label);
             }
         }
     }
-    this->minCost[0][0] = 0;
+
+    std::vector<std::vector<int64_t> > negativeQPaths;  // qpaths given by the edges in it
+
+    // for each vertex v get the least costly walk that reaches v using total demand exactly C
+    // if it has a negative reduced cost, add it to the list of negative reduced cost paths
+    for (int64_t v : this->instance.getClientIdxs()) {
+        // get least costly label for vertex v
+        const Label* best;
+        double bestCost = std::numeric_limits<double>::infinity();
+
+        for (int64_t d = 0; d <= this->C; d++) {
+            if (!this->M[d][v].empty()) {
+                const Label* label = this->M[d][v].getBetter();
+                double cost =
+                    label->cost + prices[instance.getEdgeId(label->vertex, 0)];  // adds the cost to return to the depot
+
+                if (cost < bestCost) {
+                    bestCost = cost;
+                    best = label;
+                }
+            }
+        }
+
+        // if it has a negative reduced cost, add it to the list of negative reduced cost paths
+        if (best->cost > -1e-6) return;
+
+        negativeQPaths.emplace_back();
+        auto& qPathEdges = negativeQPaths.back();
+        qPathEdges.push_back(instance.getEdgeId(best->vertex, 0));
+
+        for (const Label* current = best; current = current->previous; current->previous != nullptr) {
+            qPathEdges.push_back(instance.getEdgeId(current->previous->vertex, current->vertex));
+        }
+    }
+
+    return negativeQPaths;
 }
 
 void Pricing::initialize() {
-    // Initialize minCost and before
     for (int64_t d = 0; d <= this->C; d++) {
         for (int64_t v = 0; v < this->V; v++) {
-            this->minCost[d][v] = std::numeric_limits<double>::infinity();
-            this->before[d][v] = -1;
+            this->M[d][v].clear();
         }
     }
-    this->minCost[0][0] = 0;
+    this->M[0][0].add(0, new Label({0, 0, nullptr}));
 }
--- src/Pricing.h
+++ src/Pricing.h
@@ -7,26 +7,50 @@
 
 #include "Instance.h"
 
+struct Label {
+    const int64_t vertex;
+    const double cost;
+    const Label* previous;
+};
+
+class Labels {
+   public:
+    Labels(int64_t vertex, int64_t maxLabels = 2);
+    ~Labels();
+
+    // add the corresponding label if it has space or if it is better than any other label
+    void add(double cost, const Label* previous);
+
+    const Label* operator[](size_t index) const { return this->labels[index]; }
+
+    const Label* getBetter() const { return this->labels.front(); }
+
+    const Label* getBestLabelToExtendTo(int64_t vertex) const;
+
+    size_t size() const { return this->labels.size(); }
+    void clear();
+    bool empty() const { return this->labels.empty(); }
+
+   private:
+    const int64_t vertex;
+    const int64_t maxLabels;
+
+    std::vector<Label*> labels;
+};
+
 class Pricing {
    public:
     Pricing(const Instance& instance);
 
-    void solve(const std::vector<double>& prices);
-
-    double getMinCost(int64_t d, int64_t v) const { return this->minCost[d][v]; }
-
-    int64_t getBefore(int64_t d, int64_t v) const { return this->before[d][v]; }
+    std::vector<std::vector<int64_t> > solve(const std::vector<double>& prices);
 
    private:
     const Instance& instance;
     const int64_t V;  // Number of vertices (depot + V-1 customers)
     const int64_t C;  // Vehicle capacity
 
-    // minCost[d][v] = least costly walk that reaches vertex v using total demand exactly d
-    std::vector<std::vector<double> > minCost;
-
-    // before[d][v] = vertex visited before v in least costly walk
-    std::vector<std::vector<int64_t> > before;
+    // M[d][v] = least costly walks that reaches vertex v using total demand exactly d
+    std::vector<std::vector<Labels> > M;
 
     void initialize();
 };
