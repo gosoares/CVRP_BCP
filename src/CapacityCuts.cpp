27efd288974f3ca64597a8eea085657cbd255a4d
Gabriel Soares <gabriel@gosoares.com>
2025-01-25 00:29:11 -0300

    fix capacity cut separation call

--- src/CapacityCuts.cpp
+++ src/CapacityCuts.cpp
@@ -4,9 +4,9 @@ CapacityCuts::CapacityCuts(const Instance& instance, MasterModel& masterModel)
     : instance(instance)
     , masterModel(masterModel)
     , demands(new long long[instance.getV()])
-    , edgeTail(new long long[instance.getNbEdges()])
-    , edgeHead(new long long[instance.getNbEdges()])
-    , edgeX(new double[instance.getNbEdges()]) {
+    , edgeTail(new long long[instance.getNbEdges() + 1])
+    , edgeHead(new long long[instance.getNbEdges() + 1])
+    , edgeX(new double[instance.getNbEdges() + 1]) {
     CMGR_CreateCMgr(&(this->existingCuts), 100);
     CMGR_CreateCMgr(&(this->newCuts), 100);
 
@@ -28,15 +28,16 @@ bool CapacityCuts::separate(const std::vector<double>& x) {
     char integerAndFeasible;
     double maxViolation;
     long long nEdges = 0;  // number of edges with non-zero reduced cost
+    const int64_t depotId = this->instance.getV();
 
     for (int64_t e : this->instance.getEdgeIdxs()) {
         if (x[e] < this->tolerance) continue;
 
-        const auto& edge = this->instance.getEdge(e);
-        this->edgeTail[nEdges] = edge.first;
-        this->edgeHead[nEdges] = edge.second;
-        this->edgeX[nEdges] = x[e];
         nEdges++;
+        const auto& edge = this->instance.getEdge(e);
+        this->edgeTail[nEdges] = edge.second == 0 ? depotId : edge.second;
+        this->edgeHead[nEdges] = edge.first == 0 ? depotId : edge.first;
+        this->edgeX[nEdges] = x[e];
     }
 
     CAPSEP_SeparateCapCuts(
@@ -60,7 +61,23 @@ bool CapacityCuts::separate(const std::vector<double>& x) {
 
     std::cout << "Capacity Cuts Found: " << this->newCuts->Size << std::endl;
     std::cout << "Existing Capacity Cuts: " << this->existingCuts->Size << std::endl;
+    std::cout << "Max Violation: " << maxViolation << std::endl;
+
     this->addCuts();
+
+    // for (int64_t c = 0; c < this->newCuts->Size; c++) {
+    //     auto cut = this->newCuts->CPL[c];
+
+    //     double lhs = 0;
+    //     for (int64_t i = 1; i < cut->IntListSize; i++) {
+    //         int64_t tail = cut->IntList[i];
+    //         for (int64_t j = i + 1; j < cut->IntListSize; j++) {
+    //             lhs += x[this->instance.getEdgeId(tail, cut->IntList[j])];
+    //         }
+    //     }
+
+    //     std::cout << "Cut " << c << ": " << lhs << " <= " << cut->RHS << std::endl;
+    // }
 
     // Move new cuts to existing cuts
     for (int64_t i = 0; i < this->newCuts->Size; i++) {
